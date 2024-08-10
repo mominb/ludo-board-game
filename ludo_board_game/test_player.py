8f63757d5b5c2f6f3ddac7b4f71ff6466cdf07ee
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-10 09:01:11 +0000

    Sat, Aug 10, 2024, 9:01 AM +00:00

--- ludo_board_game/test_player.py
+++ ludo_board_game/test_player.py
@@ -4,4 +4,5 @@ import unittest
 class Test_game(unittest.TestCase):
     def setUp(self):
         self.game = Game(4)
-    
+    def test_pawns_at(self):
+        self.assertEqual(self.game.players[0].pawns_at(), [0,0,0,0])
