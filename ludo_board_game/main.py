a0aebb9d3f8b1cf8df559c8127cbbb4d9d2a7626
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-10 09:26:48 +0000

    Sat, Aug 10, 2024, 9:26 AM +00:00

--- ludo_board_game/main.py
+++ ludo_board_game/main.py
@@ -1,10 +1,10 @@
 from game import Game
 import sys
 
-game = None
+
 def main():
     game = Game(get_players())
-    
+    run_game(game)
 
 def get_players():
     try:
@@ -15,7 +15,8 @@ def get_players():
     except ValueError:
         sys.exit("Value must be integer ranging from 1 - 4")
     return num_of_players
-
+def run_game(game):
+    print
 
 if __name__ == "__main__":
     main()
