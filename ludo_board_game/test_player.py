23f27053ec6382d0bb8e7d22dd7191e25bc7e03c
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-10 09:08:02 +0000

    Sat, Aug 10, 2024, 9:08 AM +00:00

--- ludo_board_game/test_player.py
+++ ludo_board_game/test_player.py
@@ -1,8 +1,12 @@
 from game import Game
 import unittest
 
+
 class Test_game(unittest.TestCase):
     def setUp(self):
         self.game = Game(4)
-    def test_pawns_at(self):
-        print(self.game.players[0].pawns_at(13))
+
+    def test_pawn_positions_on_board(self):
+        self.game.players[0].pawns[2].position = 17
+        self.assertEqual(
+            self.game.players[0].pawn_positions_on_board(), [0, 0, 17, 0])
