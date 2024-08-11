dd41c8750c89d0293261d1e40ce79af37c5cca39
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-11 11:31:55 +0000

    Sun, Aug 11, 2024, 11:31 AM +00:00

--- ludo_board_game/main.py
+++ ludo_board_game/main.py
@@ -5,10 +5,15 @@ p = inflect.engine()
 
 
 def main():
+    ...
 def get_num_players():
+    number = int(input("How many players? ").strip())
 def ask_to_open():
-def open_pawn():
-def
+    if game.active_player.
+def ask_to_roll():
+    ...
+def run_game():
+    ...
 
 
 if __name__ == "__main__":
