4d8eb9832b3bc8350b6f17ce347c06c09563ac56
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-11 11:26:30 +0000

    Sun, Aug 11, 2024, 11:26 AM +00:00

--- ludo_board_game/main.py
+++ ludo_board_game/main.py
@@ -1,40 +1,14 @@
 from game import Game
 import sys
 import inflect
-
 p = inflect.engine()
 
 
 def main():
-    game = Game(get_players())
-    run_game(game)
-
-
-def get_players():
-    try:
-        num_of_players = int(input("How many players? ").strip())
-        if num_of_players > 4:
-            raise ValueError
-
-    except ValueError:
-        sys.exit("Value must be integer ranging from 1 - 4")
-    return num_of_players
-
-
-def run_game(game):
-
-    print(f"Player # {game.active_player.number} please roll the dice")
-    while game.dice.can_roll() and not game.dice.voided():
-        _ = input("press Enter to roll").strip()
-
-        num = game.dice.roll()
-        print(f"You rolled a {p.number_to_words(num)} !!!")
-    if game.dice.voided():
-        print("Your turn was voided :(")
-        game.change_turn()
-    if game.dice.can_open_pawn() and not game.dice.voided():
-        pawn_to_open = int(
-            input('Which pawn would you like to open? ').strip())
+def get_num_players():
+def ask_to_open():
+def open_pawn():
+def
 
 
 if __name__ == "__main__":
