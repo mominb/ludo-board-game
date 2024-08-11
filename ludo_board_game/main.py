3ebf5d44fcebe5bfc855564a76f4221177eb5c9f
Muhammad M Babar <muhammd.m.babar@gmail.com>
2024-08-11 11:37:08 +0000

    Sun, Aug 11, 2024, 11:37 AM +00:00

--- ludo_board_game/main.py
+++ ludo_board_game/main.py
@@ -1,21 +1,27 @@
 from game import Game
 import sys
 import inflect
+
 p = inflect.engine()
 
 
-def main():
-    ...
+def main(): ...
 def get_num_players():
     number = int(input("How many players? ").strip())
+
+
 def ask_to_open():
-    if game.active_player.
-def ask_to_roll():
-    ...
-def run_game():
-    ...
+    if game.active_player.pawn_positions_on_board == []:
+        pawn_to_open = int(input("which pawn do you want to open? ").strip())
+    else:
+        open = input("would you like to open a pawn? ").strip().lower()
+    if open == "yes":
+        pawn_to_open = int(input("which pawn do you want to open? ").strip())
+    else
+
+def ask_to_roll(): ...
+def run_game(): ...
 
 
 if __name__ == "__main__":
     main()
-
